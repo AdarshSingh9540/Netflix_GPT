2d1992f8d683957dd7489ae930b9f627ad5de5e3
Adarsh <adarsh9540984202@gmail.com>
2024-03-03 20:07:19 +0530

    chnage karo

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,10 +58,10 @@ const user = useSelector((store)=>store.user);
     <img  className='w-44 mx-auto md:mx-0' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
 
     {
-      user &&  <div className="flex p-2 justify-between"> 
+      user &&  <div className="flex m-1 md:p-2 justify-between"> 
       {
         showGptSearch &&
-        <select className='p-2 bg-gray-800 text-white m-4' name="" id="" onClick={handleLangChange}>
+        <select className='p-1 md:p-2 bg-gray-800 text-white m-2 md:m-4' name="" id="" onClick={handleLangChange}>
       {Supported_Lang.map(lang =>   <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
       </select>
       }
